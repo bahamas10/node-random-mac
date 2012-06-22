43bbaeb48b85301d87510e2d3c2c1a2da07738cd
Dave Eddy <dave@daveeddy.com>
2012-06-22 13:50:16 -0700

    Adding better tests

--- tests/random-mac.js
+++ tests/random-mac.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 var randomMac = require('../index'),
-    test = require('tap').test;
+    assert = require('assert'),
+    mac_regex = /^([a-fA-F0-9]{2}:){5}[0-9a-fA-F]{2}$/,
+    mac_length = 17,
+    mac1 = randomMac(),
+    mac2 = randomMac('00:11:22'),
+    mac3 = randomMac('wrong');
 
-test('Ensure Length', function(t) {
-  var mac = randomMac();
-  t.equal(mac.length, 17, 'Mac Address is 17 Characters');
-  t.end();
-});
+console.log('Testing correct length');
+assert.equal(mac1.length, mac_length);
+assert.equal(mac2.length, mac_length);
+
+console.log('Testing correct regex');
+assert.ok(mac1.match(mac_regex));
+assert.ok(mac2.match(mac_regex));
+
+console.log('Testing incorrect length');
+assert.notEqual(mac3.length, mac_length);
+
+console.log('Testing incorrect regex');
+assert.equal(mac3.match(mac_regex), null);
